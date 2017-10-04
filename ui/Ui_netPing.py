e2d78d2429289205d66499555770f789c4ffb625
evgs89 <evgs89@gmail.com>
2017-10-04 16:18:11 +0300

    на данный момент всё ок, ставим на Арены

---
+++ ui/Ui_netPing.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '/home/es89/Dropbox/Projects/Python/NetPing/ui/netPing.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(253, 311)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/1/icons/notification.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        MainWindow.setDocumentMode(False)
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralWidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.indicatorsLayout = QtWidgets.QVBoxLayout()
+        self.indicatorsLayout.setObjectName("indicatorsLayout")
+        self.verticalLayout.addLayout(self.indicatorsLayout)
+        self.textEdit = QtWidgets.QTextEdit(self.centralWidget)
+        self.textEdit.setObjectName("textEdit")
+        self.verticalLayout.addWidget(self.textEdit)
+        self.statusMessage = QtWidgets.QLabel(self.centralWidget)
+        self.statusMessage.setObjectName("statusMessage")
+        self.verticalLayout.addWidget(self.statusMessage)
+        MainWindow.setCentralWidget(self.centralWidget)
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 253, 23))
+        self.menuBar.setObjectName("menuBar")
+        self.menu = QtWidgets.QMenu(self.menuBar)
+        self.menu.setObjectName("menu")
+        self.menu_3 = QtWidgets.QMenu(self.menu)
+        self.menu_3.setObjectName("menu_3")
+        self.menu_2 = QtWidgets.QMenu(self.menuBar)
+        self.menu_2.setObjectName("menu_2")
+        MainWindow.setMenuBar(self.menuBar)
+        self.settingsAction = QtWidgets.QAction(MainWindow)
+        self.settingsAction.setObjectName("settingsAction")
+        self.restartComAction = QtWidgets.QAction(MainWindow)
+        self.restartComAction.setObjectName("restartComAction")
+        self.restartModemAction = QtWidgets.QAction(MainWindow)
+        self.restartModemAction.setCheckable(True)
+        self.restartModemAction.setObjectName("restartModemAction")
+        self.restartAllAction = QtWidgets.QAction(MainWindow)
+        self.restartAllAction.setObjectName("restartAllAction")
+        self.disableCommandsAction = QtWidgets.QAction(MainWindow)
+        self.disableCommandsAction.setObjectName("disableCommandsAction")
+        self.enableCommandsAction = QtWidgets.QAction(MainWindow)
+        self.enableCommandsAction.setObjectName("enableCommandsAction")
+        self.clearLogAction = QtWidgets.QAction(MainWindow)
+        self.clearLogAction.setObjectName("clearLogAction")
+        self.sendCommand3 = QtWidgets.QAction(MainWindow)
+        self.sendCommand3.setObjectName("sendCommand3")
+        self.sendCommand5 = QtWidgets.QAction(MainWindow)
+        self.sendCommand5.setObjectName("sendCommand5")
+        self.viewTempLogAction = QtWidgets.QAction(MainWindow)
+        self.viewTempLogAction.setObjectName("viewTempLogAction")
+        self.menu_3.addAction(self.sendCommand3)
+        self.menu_3.addAction(self.sendCommand5)
+        self.menu.addAction(self.restartComAction)
+        self.menu.addSeparator()
+        self.menu.addAction(self.restartModemAction)
+        self.menu.addAction(self.restartAllAction)
+        self.menu.addAction(self.disableCommandsAction)
+        self.menu.addAction(self.enableCommandsAction)
+        self.menu.addAction(self.menu_3.menuAction())
+        self.menu_2.addAction(self.settingsAction)
+        self.menu_2.addAction(self.clearLogAction)
+        self.menu_2.addSeparator()
+        self.menu_2.addAction(self.viewTempLogAction)
+        self.menuBar.addAction(self.menu.menuAction())
+        self.menuBar.addAction(self.menu_2.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Work with controller"))
+        self.statusMessage.setText(_translate("MainWindow", "Система активна"))
+        self.menu.setTitle(_translate("MainWindow", "Послать команду"))
+        self.menu_3.setTitle(_translate("MainWindow", "Другие (резерв)"))
+        self.menu_2.setTitle(_translate("MainWindow", "Дополнительно"))
+        self.settingsAction.setText(_translate("MainWindow", "Настройки"))
+        self.restartComAction.setText(_translate("MainWindow", "Перезапустить COM"))
+        self.restartModemAction.setText(_translate("MainWindow", "Перезагрузить модем (2)"))
+        self.restartAllAction.setText(_translate("MainWindow", "Полный перезапуск комплекса (4)"))
+        self.disableCommandsAction.setText(_translate("MainWindow", "Отключить приём команд (6)"))
+        self.enableCommandsAction.setText(_translate("MainWindow", "Включить приём команд (7)"))
+        self.clearLogAction.setText(_translate("MainWindow", "Очистить лог"))
+        self.sendCommand3.setText(_translate("MainWindow", "3"))
+        self.sendCommand5.setText(_translate("MainWindow", "5"))
+        self.viewTempLogAction.setText(_translate("MainWindow", "Просмотреть лог температуры"))
+
+import ui.icons_rc
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
---
+++ ui/netPing.py
@@ -0,0 +1,398 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module implementing MainWindow.
+"""
+
+from PyQt5.QtCore import pyqtSlot, pyqtSignal
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QLabel
+from PyQt5.QtGui import QTextCursor
+from PyQt5.Qt import QObject
+from time import sleep
+import serial, configparser, os, threading
+from datetime import datetime
+
+from .Ui_netPing import Ui_MainWindow
+from ui.tempLogView import tempLogView
+from ui.netPingSettings import netPingSettings
+from ui.extClasses import pinger
+"""
+раз в секунду принимаем от нетпинга в ASCII знак и две цифры температуры
+раз в секунду отправляем 11
+22 - перезагрузить модем (200 раз - модем перезагрузится)
+44 - перезагрузка всего (или арены)
+66 - отключить приём команд кроме 77
+77 - включить приём команд
+33 - доп реле 1 перезагрузить
+55 - доп реле 2 перезагрузить
+перезагрузка при превышении температуры происходит по команде с компа(44)
+
+"""
+
+
+class comLoop(QObject):
+    tempChangedSignal = pyqtSignal(str)
+    statusMessage = pyqtSignal(str)
+    comStateSignal = pyqtSignal(bool)
+    comState = False
+    def __init__(self, com, speed, delay = 0):
+        super(comLoop, self).__init__()
+        self.delay = delay
+        self.com = com
+        self.speed = speed
+        self.needToRebootModem = False
+    
+    def start(self):
+        if not self.needToRebootModem: self.comSendCommand = '1'
+        else: self.comSendCommand = '2'
+        self.t = threading.Thread(target = self.__comLoop, args = ())
+        self.t.daemon = True
+        self.t.start()
+
+    def settingsChanged(self, com, speed):
+        self.com = com
+        self.speed = speed
+        self.restart(3)
+    
+    def restart(self, delay=10):
+        self.statusMessage.emit(self.com + ' перезапуск, ' + str(delay) + ' сек')
+        self.comEnabled = False
+        self.delay = delay
+        print('com restart, delay = ' + str(delay))
+        self.start()
+    
+    def stop(self):
+        self.comEnabled = False
+        
+    def sendCommand(self, command):
+        self.statusMessage.emit(self.com + ' отправлена команда ' + command)
+        self.comSendCommand = command
+    
+    def __comLoop(self):
+        sleep(self.delay)
+        self.comEnabled = True
+        self.temp = '0'
+        try: 
+            self.ser = serial.Serial(self.com)
+            self.ser.baudrate = self.speed
+            self.ser.timeout = 1
+            counter = 0
+            if self.ser.readline(): self.statusMessage.emit(self.com + ' активен')
+            while self.comEnabled:
+                if self.ser.readline() == b'': 
+                    print('read empty')
+                    if self.comState:
+                        self.comStateSignal.emit(False)
+                        self.comState = False
+                    self.restart(2)
+                else:
+                    temp = self.ser.readline().decode('ascii')
+                    self.ser.write(bytes(self.comSendCommand, 'ascii'))
+                    print(temp, " ", self.comSendCommand) ##DIAG
+                    self.statusMessage.emit(self.com + ' активен, t = ' + temp)
+                    if self.comSendCommand != '1' and self.comSendCommand != '2':
+                        counter += 1
+                        if counter >= 3:
+                            if not self.needToRebootModem: self.comSendCommand = '1'
+                            else: self.comSendCommand = '2'
+                    else: 
+                        counter = 0
+                        if not self.needToRebootModem: self.comSendCommand = '1'
+                        else: self.comSendCommand = '2'
+                    if len(temp) == 3:
+                        if not self.comState:
+                            self.comState = True
+                            self.comStateSignal.emit(True)
+                        if temp != self.temp:
+                            self.temp = temp
+                            self.tempChangedSignal.emit(temp)
+            self.ser.close()
+            if self.comState: 
+                self.comStateSignal.emit(False)
+                self.comState = False
+            self.statusMessage.emit(self.com + ' закрыт')
+        except: 
+            self.ser.close()
+            print(str(Exception))
+            self.restart(1)
+        
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.watchdogEnabled = True
+        self.readConfig()
+        if self.config['logsettings']['flags'][1] == '1':
+            try: 
+                tmp = open('tmp', 'r')
+                self.logWrite((tmp.read() + ' Неожиданное выкл-е'), 1)
+                tmp.close()
+            except: pass
+            self.t1 = threading.Thread(target = self.watchdog, args = ())
+            self.t1.daemon = True
+            self.t1.start()
+        self.logWrite((datetime.now().strftime('%H:%M:%S %d/%m') + ' Запуск'), 0)
+    
+
+    def readConfig(self):
+        self.config = configparser.ConfigParser(allow_no_value = True)
+        while self.indicatorsLayout.count():
+            item = self.indicatorsLayout.takeAt(0)
+            widget = item.widget()
+            if widget is not None:
+                widget.deleteLater()
+        try:
+            self.config.read('settings.ini')
+            print('config read success, ' + self.config['comtest']['port'])
+        except:
+            print('write new config')
+            self.config['iptest'] = {}
+            self.config['iptest']['ip1'] = ''
+            self.config['iptest']['ip2'] = ''
+            self.config['comtest'] = {}
+            self.config['comtest']['enabled'] = 'False'
+            self.config['comtest']['port'] = 'COM9'
+            self.config['comtest']['speed'] = '1200'
+            self.config['comtest']['maxtemp'] = '70'
+            self.config['logsettings'] = {}
+            self.config['logsettings']['onsysup'] = 'True'
+            self.config['logsettings']['onsysdown'] = 'True'
+            self.config['logsettings']['ip1'] = 'False'
+            self.config['logsettings']['ip2'] = 'False'
+            self.config['logsettings']['com'] = 'False'
+            self.config['logsettings']['tempchange'] = 'False'
+            self.config['logsettings']['flags'] = '110000'
+            self.config['modemsettings'] = {}
+            self.config['modemsettings']['ip1'] = 'False'
+            self.config['modemsettings']['ip2'] = 'False'
+            configfile = open('settings.ini', 'w')
+            self.config.write(configfile)
+            configfile.close()
+        self.needToRebootModem1 = False
+        self.needToRebootModem2 = False
+        self.needToRebootModem3 = False
+        if self.config.getboolean('comtest', 'enabled') == True:
+            self.comState = QLabel(self.centralWidget)
+            self.comState.setObjectName("comState")
+            self.indicatorsLayout.addWidget(self.comState)
+            self.restartComAction.setEnabled(True)
+            self.restartModemAction.setEnabled(True)
+            self.disableCommandsAction.setEnabled(True)
+            self.restartAllAction.setEnabled(True)
+            self.enableCommandsAction.setEnabled(True)
+            self.sendCommand3.setEnabled(True)  
+            self.sendCommand5.setEnabled(True)
+            self.comStateChanged('Запускаем ' + self.config['comtest']['port'])
+            try:
+                self.c.settingsChanged(self.config['comtest']['port'], self.config['comtest']['speed'])
+            except:
+                self.c = comLoop(self.config['comtest']['port'], self.config['comtest']['speed'])
+                self.c.tempChangedSignal.connect(self.tempChanged)
+                self.c.statusMessage.connect(self.comStateChanged)
+                self.c.comStateSignal.connect(self.comStateSlot)
+                self.c.start()
+        else:
+            self.restartComAction.setEnabled(False)
+            self.restartModemAction.setEnabled(False)
+            self.disableCommandsAction.setEnabled(False)
+            self.restartAllAction.setEnabled(False)
+            self.enableCommandsAction.setEnabled(False)
+            self.sendCommand3.setEnabled(False)  
+            self.sendCommand5.setEnabled(False) 
+            #self.comStateChanged('Работа с COM отключена')
+            try: 
+                self.c.stop()
+            except: pass
+        if self.config['iptest']['ip1'] != '':
+            self.ip1State = QLabel(self.centralWidget)
+            self.ip1State.setObjectName("ip1State")
+            self.indicatorsLayout.addWidget(self.ip1State)
+            self.ip1 = self.config['iptest']['ip1']
+            self.p1 = pinger(self.ip1, 5)
+            self.ip1State.setText(self.ip1 + ' не отвечает')
+            self.p1.stateChangedSignal.connect(self.ipStateChanged)
+            self.ipStateChanged( self.ip1, False)
+            self.p1.start()
+        else: 
+            try: self.p1.stop()
+            except: pass
+        if self.config['iptest']['ip2'] != '':
+            self.ip2State = QLabel(self.centralWidget)
+            self.ip2State.setObjectName("ip2State")
+            self.indicatorsLayout.addWidget(self.ip2State)
+            self.ip2 = self.config['iptest']['ip2']
+            self.p2 = pinger(self.ip2, 5)
+            self.ip1State.setText(self.ip1 + ' не отвечает')
+            self.p2.stateChangedSignal.connect(self.ipStateChanged)
+            self.ipStateChanged( self.ip2, False)
+            self.p2.start()
+        else: 
+            try: self.p2.stop()
+            except: pass
+
+        
+    def ipStateChanged(self, ip, state):
+        try:
+            if ip == self.ip1: 
+                label = self.ip1State
+                logcat = 2
+                if self.config.getboolean('modemsettings', 'ip1'):
+                    if not state and self.config.getboolean('modemsettings', 'ip1'): self.needToRebootModem1 = True
+                    if state: self.needToRebootModem1 = False
+            if ip == self.ip2:
+                label = self.ip2State
+                logcat = 3
+                if self.config.getboolean('modemsettings', 'ip2'):
+                    if not state and self.config.getboolean('modemsettings', 'ip2'): self.needToRebootModem2 = True
+                    if state: self.needToRebootModem2 = False
+        except: pass
+        if self.config.getboolean('comtest', 'enabled') == True: self.c.needToRebootModem = bool(self.needToRebootModem1 + self.needToRebootModem2 + self.needToRebootModem3)
+        dt = datetime.now().strftime('%H:%M:%S %d/%m')
+        if state: text = ip + ' доступен'
+        else: text = ip + ' недоступен'
+        label.setText(text)
+        text = dt + ' ' + text
+        self.logWrite(text, logcat)
+    
+    def logWrite(self, text, category):
+        self.statusMessage.setText(text)
+        if category == 5 and self.config['logsettings']['flags'][5] == '0':
+            try: log = open('templog.txt', 'x')
+            except: log = open('templog.txt', 'a')
+        else:
+            try: log = open('log.txt', 'x')
+            except: log = open('log.txt', 'a')
+        if self.config['logsettings']['flags'][category] == '1' or category == 5: log.write(text + '\n')
+        log.close()
+        log = open('log.txt', 'r')
+        self.textEdit.setPlainText(log.read())
+        self.cur = self.textEdit.textCursor()
+        self.cur.movePosition(QTextCursor.End)
+        self.textEdit.setTextCursor(self.cur)
+        log.close()
+        
+    def watchdog(self):
+        while self.watchdogEnabled:
+            tmpfile = open('tmp', 'w')
+            tmpfile.write(datetime.now().strftime('%H:%M %d/%m'))
+            tmpfile.close()
+            self.statusMessage.setText(datetime.now().strftime('%H:%M %d/%m') + ' контроль активен')
+            try: 
+                if self.c.needToRebootModem: self.statusMessage.setText(datetime.now().strftime('%H:%M %d/%m') + ' ждём перезагрузки модема')
+            except: pass
+            sleep(60)
+            
+    def comStateChanged(self, text):
+        self.comState.setText(text)
+        
+    def tempChanged(self, temp):
+        self.comState.setText(self.c.com + ' активен, t = ' + temp)
+        self.statusMessage.setText(datetime.now().strftime('%H:%M:%S %d/%m') + ' t = ' + temp)
+        self.logWrite((datetime.now().strftime('%H:%M:%S %d/%m') + ' t = ' + temp), 5)
+        if int(temp[1:]) >= int(self.config['comtest']['maxtemp']): 
+            self.logWrite((datetime.now().strftime('%H:%M:%S %d/%m') + ' ПРЕВЫШЕНИЕ КРИТ. ТЕМПЕРАТУРЫ'), 4)
+            self.c.sendCommand('4')
+        
+    def comStateSlot(self, state):
+        if state: self.logWrite((datetime.now().strftime('%H:%M:%S %d/%m') + ' ' + self.c.com + ' активен'), 4)
+        else: self.logWrite((datetime.now().strftime('%H:%M:%S %d/%m') + ' ' + self.c.com + ' неактивен'), 4)
+
+    @pyqtSlot()
+    def closeEvent(self, event):
+        try: self.p1.stop()
+        except: pass
+        try: self.p2.stop()
+        except: pass
+        self.watchdogEnabled = False
+        try: os.remove('tmp')
+        except: pass
+        try: self.c.stop()
+        except: pass
+        self.logWrite((datetime.now().strftime('%H:%M:%S %d/%m') + ' Выключение'), 1)
+        event.accept()
+    
+    @pyqtSlot()
+    def on_settingsAction_triggered(self):
+        self.setWin = netPingSettings(self)
+        self.setWin.ip1Edit.setText(self.config['iptest']['ip1'])
+        self.setWin.ip2Edit.setText(self.config['iptest']['ip2'])
+        self.setWin.comPortEnabled.setChecked(self.config.getboolean('comtest', 'enabled'))
+        self.setWin.comEdit.setText(self.config['comtest']['port'])
+        self.setWin.lineEdit_4.setText(self.config['comtest']['speed'])
+        self.setWin.maxTempEdit.setText(self.config['comtest']['maxtemp'])
+        self.setWin.ip1reboot.setChecked(self.config.getboolean('modemsettings', 'ip1'))
+        self.setWin.ip2reboot.setChecked(self.config.getboolean('modemsettings', 'ip2'))
+        self.setWin.ip1Log.setChecked(self.config.getboolean('logsettings', 'ip1'))
+        self.setWin.ip2Log.setChecked(self.config.getboolean('logsettings', 'ip2'))
+        self.setWin.sysStartUp.setChecked(self.config.getboolean('logsettings', 'onsysup'))
+        self.setWin.sysShutdown.setChecked(self.config.getboolean('logsettings', 'onsysdown'))
+        self.setWin.comPortLog.setChecked(self.config.getboolean('logsettings', 'com'))
+        self.setWin.tempLog.setChecked(self.config.getboolean('logsettings', 'tempchange'))
+        self.setWin.settingsChanged.connect(self.readConfig)
+        self.setWin.show()
+        
+    
+    @pyqtSlot()
+    def on_restartComAction_triggered(self):
+        if self.config.getboolean('comtest', 'enabled'): self.c.restart(10)
+        
+    
+    @pyqtSlot()
+    def on_restartAllAction_triggered(self):
+        acq = QMessageBox.question(self, 'Запрос', 'Комплекс будет перезапущен. Вы уверены?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if acq == QMessageBox.Yes:
+            try: 
+                self.c.sendCommand('4')
+                self.logWrite((datetime.now().strftime('%H:%M:%S %d/%m') + ' Перезапуск комплекса'), 4)
+            except: pass
+            
+    @pyqtSlot()
+    def on_disableCommandsAction_triggered(self):
+        if self.config.getboolean('comtest', 'enabled'): self.c.sendCommand('6')
+    
+    @pyqtSlot()
+    def on_enableCommandsAction_triggered(self):
+        if self.config.getboolean('comtest', 'enabled'): self.c.sendCommand('7')
+    
+    @pyqtSlot()
+    def on_clearLogAction_triggered(self):
+        self.textEdit.setPlainText('')
+        log = open('log.txt', 'w')
+        log.write('')
+        log.close()
+    
+    @pyqtSlot()
+    def on_sendCommand3_triggered(self):
+        if self.config.getboolean('comtest', 'enabled'): self.c.sendCommand('3')
+    
+    @pyqtSlot()
+    def on_sendCommand5_triggered(self):
+        if self.config.getboolean('comtest', 'enabled'): self.c.sendCommand('5')
+    
+    @pyqtSlot(bool)
+    def on_restartModemAction_toggled(self, p0):
+        if self.config.getboolean('comtest', 'enabled') and p0: 
+            self.needToRebootModem3 = True
+            try: self.c.needToRebootModem = True
+            except: pass
+            self.logWrite((datetime.now().strftime('%H:%M:%S %d/%m') + ' Задан перезапуск модема'), 4)
+        if self.config.getboolean('comtest', 'enabled') and not p0: 
+            self.needToRebootModem3 = False
+            try: self.c.needToRebootModem = False
+            except: pass
+            self.logWrite((datetime.now().strftime('%H:%M:%S %d/%m') + ' Отменён перезапуск модема'), 4)
+    
+    @pyqtSlot()
+    def on_viewTempLogAction_triggered(self):
+        filename = 'templog.txt'
+        self.logViewer = tempLogView(self)
+        self.logViewer.filename = filename
+        text = ''
+        try: 
+            log = open(filename,  'r')
+            text = log.read()
+            log.close()
+        except: text = ''
+        if text == '': text = 'Log is empty'
+        self.logViewer.log.setPlainText(text)
+        self.logViewer.show()
